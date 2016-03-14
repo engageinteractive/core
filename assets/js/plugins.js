093002d225df46a715a448a5b05a0fb67ef9c152
Simon Willans <simon@engageinteractive.co.uk>
2016-03-14 09:24:18 +0000

    Moves some utilities to end of SCSS import (helpers should override specificity). File tidy-up.

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -1,5 +1,7 @@
+// Plugins
+// @codekit-prepend "plugins/jquery/simpleSelect.js"
+
 // jQuery plugins
 // @codekit-prepend "plugins/jquery/fitvids.js"
-// @codekit-prepend "plugins/jquery/simpleSelect.js"
 // @codekit-prepend "plugins/jquery/preload.js"
 // @codekit-prepend "plugins/jquery/loadImg.js"
