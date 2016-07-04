6b7f0bc8756e0bae3e1c3a6e6dec673cc0e210db
Adam Gilleard <adam.gilleard@engageinteractive.co.uk>
2016-07-04 15:02:21 +0100

    Restores grid scanning tool

---
+++ public/_includes/components/grid.php
@@ -0,0 +1,73 @@
+<div class="example-grid">
+	<h2 class="h3">Columns</h2>
+	<div class="row">
+		<div class="full-width">
+			<span>Full width</span>
+		</div>
+	</div>
+	<div class="row">
+		<div class="column tablet-12 tablet-push-12"><span>12 (Push 12)</span></div>
+		<div class="column tablet-12 tablet-pull-12"><span>12 (Pull 12)</span></div>
+	</div>
+	<div class="row">
+		<div class="column base-8"><span>8</span></div>
+		<div class="column base-8"><span>8</span></div>
+		<div class="column base-8"><span>8</span></div>
+	</div>
+	<div class="row">
+		<div class="column base-6"><span>6</span></div>
+		<div class="column base-6"><span>6</span></div>
+		<div class="column base-6"><span>6</span></div>
+		<div class="column base-6"><span>6</span></div>
+	</div>
+	<div class="row">
+		<div class="column base-4"><span>4</span></div>
+		<div class="column base-4 base-push-4"><span>4 (Push 4)</span></div>
+		<div class="column base-4 base-push-4"><span>4 (Push 4)</span></div>
+		<div class="column base-4 base-push-8"><span>4 (Push 8)</span></div>
+	</div>
+	<div class="row">
+		<div class="column base-10 base-push-13"><span>10 (Push 13)</span></div>
+		<div class="column base-10 base-pull-9"><span>10 (Pull 9)</span></div>
+	</div>
+	<h2 class="h3">Block</h2>
+	<ul class="phone-block-grid-2 tablet-block-grid-6 desktop-block-grid-8">
+		<li><span>&nbsp;</span></li>
+		<li><span>&nbsp;</span></li>
+		<li><span>&nbsp;</span></li>
+		<li><span>&nbsp;</span></li>
+		<li><span>&nbsp;</span></li>
+		<li><span>&nbsp;</span></li>
+		<li><span>&nbsp;</span></li>
+		<li><span>&nbsp;</span></li>
+	</ul>
+	<ul class="phone-block-grid-4 tablet-block-grid-8 desktop-block-grid-12">
+		<li><span>&nbsp;</span></li>
+		<li><span>&nbsp;</span></li>
+		<li><span>&nbsp;</span></li>
+		<li><span>&nbsp;</span></li>
+		<li><span>&nbsp;</span></li>
+		<li><span>&nbsp;</span></li>
+		<li><span>&nbsp;</span></li>
+		<li><span>&nbsp;</span></li>
+		<li><span>&nbsp;</span></li>
+		<li><span>&nbsp;</span></li>
+		<li><span>&nbsp;</span></li>
+		<li><span>&nbsp;</span></li>
+	</ul>
+	<h2 class="h3">Flush</h2>
+	<h3 class="h4">Flush first 2</h3>
+	<div class="flush-test desktop-flush-first-2">
+		<div><span>Child 1</span></div>
+		<div><span>Child 2</span></div>
+		<div><span>Child 3</span></div>
+		<div><span>Child 4</span></div>
+	</div>
+	<h3 class="h4">Flush first 3</h3>
+	<div class="flush-test phone-flush-first-2 tablet-flush-first-3">
+		<div><span>Child 1</span></div>
+		<div><span>Child 2</span></div>
+		<div><span>Child 3</span></div>
+		<div><span>Child 4</span></div>
+	</div>
+</div>
