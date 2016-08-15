e61c5ce1423ce033f3d44743ad232608dc4ece36
Simon Willans <simon@engageinteractive.co.uk>
2016-08-15 11:27:46 +0100

    Message icon svg sprite fix.

--- public/styleguide/components/module/message.php
+++ public/styleguide/components/module/message.php
@@ -2,10 +2,14 @@
 	<div class="message">
 		<h2 class="h4">
 			<span class="icon-text">
-				<span class="icon-text__icon icon-wrapper icon-circle icon-circle--green"><i class="icon icon-arrow-down"></i></span>
-				<span class="icon-text__text">Important action required</span>
+				<svg viewBox="0 0 100 100" class="icon-text__i icon">
+					<use xlink:href="/assets/img/svg-icons/sprite.svg#cross"></use>
+				</svg>
+
+				<span class="icon-text__t">Important action required</span>
 			</span>
 		</h2>
+
 		<p>Neutral message: Lorem ipsum dolor sit amet, <a href="#">consectetur adipisicing</a> elit. Facilis quae earum asperiores voluptatem quod numquam, minima illum eveniet quisquam, rerum, ratione molestiae eos, eaque hic optio laborum molestias dolorem commodi.</p>
 	</div>
 </div>
@@ -13,7 +17,9 @@
 <div class="media">
 	<div class="message message--positive">
 		<h2 class="h4">Message title/info</h2>
+
 		<p>Positive message: Lorem ipsum dolor sit amet, <a href="#">consectetur adipisicing</a> elit. Facilis quae earum asperiores voluptatem quod numquam, minima illum eveniet quisquam, rerum, ratione molestiae eos, eaque hic optio laborum molestias dolorem commodi.</p>
+
 		<p>Positive message: Lorem ipsum dolor sit amet, <a href="#">consectetur adipisicing</a> elit. Facilis quae earum asperiores voluptatem quod numquam, minima illum eveniet quisquam, rerum, ratione molestiae eos, eaque hic optio laborum molestias dolorem commodi.</p>
 	</div>
 </div>
