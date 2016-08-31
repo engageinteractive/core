7dd7eb305d06e971946abf40853d5eea217b1e04
Adam Gilleard <adam.gilleard@engageinteractive.co.uk>
2016-08-31 15:50:06 +0100

    Updates styleguide page.

--- public/styleguide/index.php
+++ public/styleguide/index.php
@@ -2,6 +2,37 @@
 include_once('functions.php');
 $styleguide = true;
 $meta_title = 'Styleguide';
+$sections = [
+	[
+		'foundation',
+		'Branding, colour schemes and basic typopgraphy.',
+		false
+	],
+	[
+		'core',
+		'Basic low level elements such as links and buttons.'
+	],
+	[
+		'module',
+		'Simple combinations of cores and reusable components not tied to a specific section'
+	],
+	[
+		'layout',
+		'Distinct section of an interface.'
+	],
+	[
+		'grid',
+		'For responsive web layouts.'
+	],
+	[
+		'utility',
+		'Classes and helpers that override other styles.'
+	],
+	[
+		'scripts',
+		'Demos of any useful baseplate scripts.'
+	],
+];
 include('../_includes/header.php');
 ?>
 
@@ -14,24 +45,12 @@ include('../_includes/header.php');
 
 			<ul>
 				<?php sgListOptions('documentation', false); ?>
-
 				<li>
-					<span>Components</span>
-
+					Components
 					<ul>
-						<?php sgListOptions('foundation'); ?>
-
-						<?php sgListOptions('core'); ?>
-
-						<?php sgListOptions('module'); ?>
-
-						<?php sgListOptions('layout'); ?>
-
-						<?php sgListOptions('grid'); ?>
-
-						<?php sgListOptions('utility'); ?>
-
-						<?php sgListOptions('scripts'); ?>
+						<?php foreach ($sections as $section): ?>
+							<?php sgListOptions($section[0]); ?>
+						<?php endforeach; ?>
 					</ul>
 				</li>
 			</ul>
@@ -50,89 +69,21 @@ include('../_includes/header.php');
 		<section class="sg-group sg-group--components">
 			<h1 class="sg-group__title h1">Components</h1>
 
-			<section class="sg-section">
-				<header class="sg-section__header">
-					<h1 class="h2">Foundation</h1>
+			<?php foreach ($sections as $section): ?>
 
-					<p>Branding, colour schemes and basic typopgraphy.</p>
-				</header>
+				<section class="sg-section">
+					<header class="sg-section__header">
+						<h1 class="h2"><?= ucfirst($section[0]) ?></h1>
 
-				<div class="sg-section__body">
-					<?php sgContent('foundation', 'components', $showMarkup = false); ?>
-				</div>
-			</section>
+						<p><?= ucfirst($section[1]) ?></p>
+					</header>
 
-			<section class="sg-section">
-				<header class="sg-section__header">
-					<h1 class="h2">Core</h1>
+					<div class="sg-section__body">
+						<?php sgContent($section[0], 'components', isset($section[2]) ? $section[2] : true); ?>
+					</div>
+				</section>
 
-					<p>Basic low level elements such as links and buttons.</p>
-				</header>
-
-				<div class="sg-section__body">
-					<?php sgContent('core'); ?>
-				</div>
-			</section>
-
-			<section class="sg-section">
-				<header class="sg-section__header">
-					<h1 class="h2">Module</h1>
-
-					<p>Simple combinations of cores and reusable components not tied to a specific section</p>
-				</header>
-
-				<div class="sg-section__body">
-					<?php sgContent('module'); ?>
-				</div>
-			</section>
-
-			<section class="sg-section">
-				<header class="sg-section__header">
-					<h1 class="h2">Layout</h1>
-
-					<p>Distinct section of an interface.</p>
-				</header>
-
-				<div class="sg-section__body">
-					<?php sgContent('layout'); ?>
-				</div>
-			</section>
-
-			<section class="sg-section">
-				<header class="sg-section__header">
-					<h1 class="h2">Grid</h1>
-
-					<p>For responsive web layouts.</p>
-				</header>
-
-				<div class="sg-section__body">
-					<?php sgContent('grid'); ?>
-				</div>
-			</section>
-
-			<section class="sg-section">
-				<header class="sg-section__header">
-					<h1 class="h2">Utility</h1>
-
-					<p>Classes and helpers that override other styles.</p>
-				</header>
-
-				<div class="sg-section__body">
-					<?php sgContent('utility'); ?>
-				</div>
-			</section>
-
-			<section class="sg-section">
-				<header class="sg-section__header">
-					<h1 class="h2">Scripts</h1>
-
-					<p>Demos of any useful baseplate scripts.</p>
-				</header>
-
-				<div class="sg-section__body">
-					<?php sgContent('scripts'); ?>
-				</div>
-			</section>
+			<?php endforeach; ?>
 		</section>
 	</div>
 </div>
