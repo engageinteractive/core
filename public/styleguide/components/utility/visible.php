54fc4d2fb7be216adeefb51044d6a245207d31f7
Simon Willans <simon@engageinteractive.co.uk>
2016-09-02 16:15:51 +0100

    Visible classes now more BEM. Adds example to styleguide.

---
+++ public/styleguide/components/utility/visible.php
@@ -0,0 +1,9 @@
+<p>A div that's hidden on mobile, and appears at tablet</p>
+
+<div class="hidden visible--tablet" style="padding: 30px; background-color: #DDD; text-align: center; margin-bottom: 20px;">hidden--tablet visible--desktop</div>
+
+<p>A div that's visible on mobile, hidden at tablet, and visible again at desktop</p>
+
+<div class="hidden--tablet visible--desktop" style="padding: 30px; background-color: #DDD; text-align: center; margin-bottom: 20px;">hidden--tablet visible--desktop</div>
+
+<p>Visible classes can also be inline and inline-block. Just use classes such as <code>visible-inline-block--tablet</code></p>
