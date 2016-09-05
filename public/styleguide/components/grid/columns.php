05836bc2f1acbcbaf146e9980a5fb78afdbed04d
Simon Willans <simon@engageinteractive.co.uk>
2016-09-05 10:59:59 +0100

    Width, push and pull now separated from columns file.

--- public/styleguide/components/grid/columns.php
+++ public/styleguide/components/grid/columns.php
@@ -1,31 +1,31 @@
 <div class="sg-example-grid">
 	<div class="row flush-first-1 flush-first-2--tablet">
-		<div class="column tablet-12"><span>12 (Push 12)</span></div>
-		<div class="column tablet-12"><span>12 (Pull 12)</span></div>
+		<div class="column width--tablet-12"><span>12 (tablet)</span></div>
+		<div class="column width--tablet-12"><span>12 (tablet)</span></div>
 	</div>
 
 	<div class="row">
-		<div class="column base-8"><span>8</span></div>
-		<div class="column base-8"><span>8</span></div>
-		<div class="column base-8"><span>8</span></div>
+		<div class="column width--8"><span>8</span></div>
+		<div class="column width--8"><span>8</span></div>
+		<div class="column width--8"><span>8</span></div>
 	</div>
 
 	<div class="row">
-		<div class="column base-6"><span>6</span></div>
-		<div class="column base-6"><span>6</span></div>
-		<div class="column base-6"><span>6</span></div>
-		<div class="column base-6"><span>6</span></div>
+		<div class="column width--6"><span>6</span></div>
+		<div class="column width--6"><span>6</span></div>
+		<div class="column width--6"><span>6</span></div>
+		<div class="column width--6"><span>6</span></div>
 	</div>
 
 	<div class="row">
-		<div class="column base-4"><span>4</span></div>
-		<div class="column base-4 base-push-4"><span>4 (Push 4)</span></div>
-		<div class="column base-4 base-push-4"><span>4 (Push 4)</span></div>
-		<div class="column base-4 base-push-8"><span>4 (Push 8)</span></div>
+		<div class="column width--4"><span>4</span></div>
+		<div class="column width--4 push--4"><span>4 (Push 4)</span></div>
+		<div class="column width--4 push--4"><span>4 (Push 4)</span></div>
+		<div class="column width--4 push--8"><span>4 (Push 8)</span></div>
 	</div>
 
 	<div class="row">
-		<div class="column base-10 base-push-13"><span>10 (Push 13)</span></div>
-		<div class="column base-10 base-pull-9"><span>10 (Pull 9)</span></div>
+		<div class="column width--10 push--13"><span>10 (Push 13)</span></div>
+		<div class="column width--10 pull--9"><span>10 (Pull 9)</span></div>
 	</div>
 </div>
--- public/styleguide/components/scripts/equal-height.php
+++ public/styleguide/components/scripts/equal-height.php
@@ -2,7 +2,7 @@
 
 <div class="equal-height" data-active="768">
 	<div class="row">
-		<div class="column tablet-12">
+		<div class="column width--12">
 			<div class="equal-height__component" style="background: #EEE;">
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea culpa pariatur aperiam sed blanditiis perspiciatis eius, iusto maxime esse corrupti, quis eaque numquam eum est a et amet aliquid dolorem.</p>
 
@@ -12,7 +12,7 @@
 			</div>
 		</div>
 
-		<div class="column tablet-12">
+		<div class="column width--12">
 			<div class="equal-height__component" style="background: #EEE;">
 				<p>Hello world.</p>
 			</div>
