7c81f83d3edb4928a047f42f45a755e788157529
Simon Willans <simon@engageinteractive.co.uk>
2016-09-05 13:58:30 +0100

    Adds flush-first classes to equal height example.

--- public/styleguide/components/scripts/equal-height.php
+++ public/styleguide/components/scripts/equal-height.php
@@ -1,7 +1,7 @@
 <p>Sometimes we might want content blocks to be of equal heights (and you don't want to use flexbox)</p>
 
 <div class="equal-height" data-active="768">
-	<div class="row">
+	<div class="row flush-first--2">
 		<div class="column width--12">
 			<div class="equal-height__component" style="background: #EEE;">
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea culpa pariatur aperiam sed blanditiis perspiciatis eius, iusto maxime esse corrupti, quis eaque numquam eum est a et amet aliquid dolorem.</p>
