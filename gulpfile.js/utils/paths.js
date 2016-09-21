d194c2a92b38080a548b5a535d74ba5bb6cc211c
Adam Gilleard <adam.gilleard@engageinteractive.co.uk>
2016-09-21 11:29:17 +0100

    Updates paths util to support extension splits

--- gulpfile.js/utils/paths.js
+++ gulpfile.js/utils/paths.js
@@ -3,12 +3,16 @@ var
 	path = require('path');
 
 module.exports = function(task) {
+	var extensions = config.tasks[task].extensions;
+
 	return {
-		src: path.join(
-			config.root.src,
-			config.tasks[task].src,
-			'/**/*.+(' + config.tasks[task].extensions.join('|') + ')'
-		),
+		src: function(type) {
+			return path.join(
+				config.root.src,
+				config.tasks[task].src,
+				'/**/*.+(' + (type ? extensions[type] : extensions).join('|') + ')'
+			);
+		},
 		dest: path.join(
 			config.root.dest,
 			config.tasks[task].dest
