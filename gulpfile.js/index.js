9e2b3cf817ffca15c2360d42c9d1af4583172c6b
Adam Gilleard <adam.gilleard@engageinteractive.co.uk>
2016-09-23 08:57:30 +0100

    Add support for old task names

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -3,3 +3,9 @@ var gulp = require('gulp');
 require('require-dir')('./tasks', { recurse: true });
 
 gulp.task('default', gulp.parallel(require('./config').tasks.default.tasks));
+
+// legacy faux aliases
+
+gulp.task('styles', gulp.series('css'));
+gulp.task('scripts.lint', gulp.series('scripts.site.lint'));
+gulp.task('scripts.lint.full', gulp.series('scripts.site.lint.full'));
