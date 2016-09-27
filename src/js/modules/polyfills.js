4c05653827c708ae1ad96285fec79b273460ae09
Simon Willans <simon@engageinteractive.co.uk>
2016-09-27 15:17:01 +0100

    Removes simpleSelect.
    Now here: https://github.com/engageinteractive/core-simpleSelect

--- src/js/modules/polyfills.js
+++ src/js/modules/polyfills.js
@@ -1,13 +1,1 @@
 require('svg4everybody')();
-
-if (!Modernizr.csspointerevents) {
-
-	require.ensure('./simpleSelect', function(require) {
-
-		var simpleSelect = require('./simpleSelect');
-
-		simpleSelect($('.select select:not([multiple="multiple"])'));
-
-	});
-
-}
