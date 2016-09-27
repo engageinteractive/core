1b82c7ef1c466e3183b26d3919e59c6f947c6595
Simon Willans <simon@engageinteractive.co.uk>
2016-09-27 15:17:48 +0100

    Reference all available plugins/modules.

--- src/js/site.js
+++ src/js/site.js
@@ -1,3 +1,8 @@
+// require('./plugins/loadImg');
+// require('./modules/bem');
 require('./modules/form-focus');
 require('./modules/links');
 require('./modules/polyfills');
+// require('./modules/scroll');
+// require('./modules/utils');
+// require('./modules/viewport');
