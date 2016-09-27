d4a1b21a6b6756963e14e6f0cff20dd915f36d7e
Adam Gilleard <adam.gilleard@engageinteractive.co.uk>
2016-09-27 17:13:57 +0100

    Updates default js.

--- src/js/site.js
+++ src/js/site.js
@@ -1,8 +1,5 @@
 // require('./plugins/loadImg');
-// require('./modules/bem');
+
 require('./modules/form-focus');
 require('./modules/links');
 require('./modules/polyfills');
-// require('./modules/scroll');
-// require('./modules/utils');
-// require('./modules/viewport');
