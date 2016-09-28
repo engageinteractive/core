1ed54222ae7b0e3e663e5b50362d9284d9a50066
Simon Willans <simon@engageinteractive.co.uk>
2016-09-28 14:03:51 +0100

    Automates icon list.

--- public/styleguide/components/core/icons.php
+++ public/styleguide/components/core/icons.php
@@ -1,119 +1,16 @@
 <h2 class="h4">Basic icons</h2>
 
 <ul class="inline-children--block sg-icons">
+	<?php
+	$icons = glob('../../src/sprite/*.svg', GLOB_BRACE);
+	foreach($icons as $icon) :
+	?>
 	<li>
 		<svg class="icon">
-			<use xlink:href="/assets/img/sprite.svg#chevron-up"></use>
+			<use xlink:href="/assets/img/sprite.svg#<?php echo basename($icon, '.svg'); ?>"></use>
 		</svg>
 	</li>
-
-	<li>
-		<svg class="icon">
-			<use xlink:href="/assets/img/sprite.svg#chevron-down"></use>
-		</svg>
-	</li>
-
-	<li>
-		<svg class="icon">
-			<use xlink:href="/assets/img/sprite.svg#chevron-left"></use>
-		</svg>
-	</li>
-
-	<li>
-		<svg class="icon">
-			<use xlink:href="/assets/img/sprite.svg#chevron-right"></use>
-		</svg>
-	</li>
-
-	<li>
-		<svg class="icon">
-			<use xlink:href="/assets/img/sprite.svg#chevron-thin-up"></use>
-		</svg>
-	</li>
-
-	<li>
-		<svg class="icon">
-			<use xlink:href="/assets/img/sprite.svg#chevron-thin-down"></use>
-		</svg>
-	</li>
-
-	<li>
-		<svg class="icon">
-			<use xlink:href="/assets/img/sprite.svg#chevron-thin-left"></use>
-		</svg>
-	</li>
-
-	<li>
-		<svg class="icon">
-			<use xlink:href="/assets/img/sprite.svg#chevron-thin-right"></use>
-		</svg>
-	</li>
-
-	<li>
-		<svg class="icon">
-			<use xlink:href="/assets/img/sprite.svg#check"></use>
-		</svg>
-	</li>
-
-	<li>
-		<svg class="icon">
-			<use xlink:href="/assets/img/sprite.svg#cross"></use>
-		</svg>
-	</li>
-
-	<li>
-		<svg class="icon">
-			<use xlink:href="/assets/img/sprite.svg#plus"></use>
-		</svg>
-	</li>
-
-	<li>
-		<svg class="icon">
-			<use xlink:href="/assets/img/sprite.svg#minus"></use>
-		</svg>
-	</li>
-
-	<li>
-		<svg class="icon">
-			<use xlink:href="/assets/img/sprite.svg#link"></use>
-		</svg>
-	</li>
-
-	<li>
-		<svg class="icon">
-			<use xlink:href="/assets/img/sprite.svg#phone"></use>
-		</svg>
-	</li>
-
-	<li>
-		<svg class="icon">
-			<use xlink:href="/assets/img/sprite.svg#mail"></use>
-		</svg>
-	</li>
-
-	<li>
-		<svg class="icon">
-			<use xlink:href="/assets/img/sprite.svg#triangle-up"></use>
-		</svg>
-	</li>
-
-	<li>
-		<svg class="icon">
-			<use xlink:href="/assets/img/sprite.svg#triangle-down"></use>
-		</svg>
-	</li>
-
-	<li>
-		<svg class="icon">
-			<use xlink:href="/assets/img/sprite.svg#triangle-left"></use>
-		</svg>
-	</li>
-
-	<li>
-		<svg class="icon">
-			<use xlink:href="/assets/img/sprite.svg#triangle-right"></use>
-		</svg>
-	</li>
+	<?php endforeach; ?>
 </ul>
 
 <h2 class="h4">Inline icons</h2>
