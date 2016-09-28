ee9b7ac99a1f3c1e751a1724b11de3eec5bce5cc
Adam Gilleard <adam.gilleard@engageinteractive.co.uk>
2016-09-28 15:24:41 +0100

    Updates scripts task to avoid crashing on syntax errors.
    Merged the lint and compile tasks.
    Means the task doesn’t have to fail to avoid compilation, which proved near impossible in gulp.

--- gulpfile.js/tasks/scripts.js
+++ gulpfile.js/tasks/scripts.js
@@ -2,74 +2,79 @@ var
 	config = require('../config'),
 	notification = require('../utils/notification'),
 	paths = require('../utils/paths')('scripts'),
-	task,
 
 	ModernizrPlugin = require('modernizr-webpack-plugin'),
 	eslint = require('gulp-eslint'),
-	filter = require('gulp-filter')(['**/!(*.min.js)']),
+	filter = require('gulp-filter'),
 	gulp = require('gulp'),
 	named = require('vinyl-named'),
 	summary = require('engage-eslint-summary'),
-	webpack = require('webpack-stream');
+	webpack = require('webpack-stream'),
+
+	task = function(done) {
+		var filters = {
+			custom: filter(['**/!(*.min.js)']),
+			entries: filter([paths.entries()]),
+		};
+
+		return gulp
+			.src(paths.src)
+			.pipe(filters.custom)
+			.pipe(eslint())
+			.pipe(eslint.format(summary))
+			.pipe(eslint.failOnError()
+				.on('error', function() {
+					notification({
+						title: 'JavaScript Error',
+						subtitle: [
+							'<%= options.relative(options.cwd, error.fileName) %>',
+							'<%= error.lineNumber %>',
+						].join(':'),
+						message: '<%= error.message %>',
+						open: 'file://<%= error.fileName %>',
+					}).apply(this, arguments);
+				})
+			)
+			.pipe(eslint.failOnError()
+				.on('error', function() {
+					done();
+				})
+			)
+			.pipe(filters.entries)
+			.pipe(named())
+			.pipe(
+				webpack({
+					devtool: 'source-map',
+					output: {
+						publicPath: paths.public,
+					},
+					plugins: [
+						new webpack.webpack.optimize.UglifyJsPlugin({
+							compress: { warnings: false },
+						}),
+						new ModernizrPlugin({
+							filename: 'modernizr.js',
+							htmlWebpackPlugin: false,
+							minify: true,
+							options: [
+								'setClasses',
+							],
+							'feature-detects': config.tasks.scripts.featureDetects,
+						}),
+					],
+				})
+			)
+			.pipe(gulp.dest(paths.dest));
+	};
 
 gulp.task('scripts.lint', function() {
 	return gulp
 		.src(paths.src)
-		.pipe(filter)
 		.pipe(eslint())
-		.pipe(eslint.format(summary))
-		.pipe(
-			eslint.failOnError()
-				.on('error', notification({
-					title: 'JavaScript Error',
-					subtitle: [
-						'<%= options.relative(options.cwd, error.fileName) %>',
-						'<%= error.lineNumber %>',
-					].join(':'),
-					message: '<%= error.message %>',
-					open: 'file://<%= error.fileName %>',
-				}))
-		)
-		.pipe(eslint.failAfterError());
+		.pipe(eslint.format());
 });
 
-gulp.task('scripts.lint.full', function() {
-	return gulp
-		.src(paths.src)
-		.pipe(eslint())
-		.pipe(eslint.format())
-		.pipe(eslint.failAfterError());
-});
-
-gulp.task('scripts.compile', function() {
-	return gulp
-		.src(paths.entries())
-		.pipe(named())
-		.pipe(
-			webpack({
-				devtool: 'eval',
-				output: {
-					publicPath: paths.public,
-				},
-				plugins: [
-					new webpack.webpack.optimize.UglifyJsPlugin({
-						compress: { warnings: false },
-					}),
-					new ModernizrPlugin({
-						filename: 'modernizr.js',
-						htmlWebpackPlugin: false,
-						minify: true,
-						options: [
-							'setClasses',
-						],
-						'feature-detects': config.tasks.scripts.featureDetects,
-					}),
-				],
-			})
-		)
-		.pipe(gulp.dest(paths.dest));
-});
-
-task = gulp.series('scripts.lint', 'scripts.compile');
 gulp.task('scripts', task);
 module.exports = task;
+
+gulp.task('scripts.lint.full', gulp.series('scripts.lint')); // legacy alias
