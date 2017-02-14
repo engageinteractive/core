9e61fce7235d3b471fca1fed06c71d9043dc9c66
Adam Gilleard <adam.gilleard@engageinteractive.co.uk>
2017-02-14 10:01:17 +0000

    Lints new task.

--- gulpfile.js/tasks/favicon.js
+++ gulpfile.js/tasks/favicon.js
@@ -1,5 +1,4 @@
 const
-	config = require('../config'),
 	paths = require('../utils/paths')('favicon'),
 
 	gulp = require('gulp'),
@@ -11,15 +10,15 @@ const
 		.pipe(favicons({
 			online: true,
 			icons: {
-	            android: false,
-	            appleIcon: false,
-	            appleStartup: false,
-	            coast: false,
-	            favicons: true,
-	            firefox: false,
-	            windows: false,
-	            yandex: false
-	        }
+				android: false,
+				appleIcon: false,
+				appleStartup: false,
+				coast: false,
+				favicons: true,
+				firefox: false,
+				windows: false,
+				yandex: false
+			}
 		}))
 		.pipe(filter('favicon.ico'))
 		.pipe(gulp.dest(paths.dest));
