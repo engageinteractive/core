1810c538615e1918ad6cf988c2fd7e6e579f32c6
Adam Gilleard <adam.gilleard@engageinteractive.co.uk>
2018-01-23 15:08:24 +0000

    Refactor

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,55 +1,41 @@
 const mix = require('laravel-mix');
-const jsonImporter = require('node-sass-json-importer');
+const importer = require('node-sass-json-importer');
 
-mix.options({
-	clearConsole: false,
-});
-
-// mix.autoload({}); // TODO: check if needed
-
-mix.webpackConfig({
-	module: {
-		rules: [
-			{
-				test: /\.scss$/,
-				use: [
-					{
-						loader: 'sass-loader',
-						options: {
-							importer: jsonImporter,
-						},
-					},
-				],
+mix
+	.browserSync({
+		files: 'public/assets',
+		ghostMode: false,
+		notify: false,
+		open: false,
+		port: 5757,
+		proxy: 'core.dev.com',
+	})
+	.options({
+		autoprefixer: {
+			options: {
+				// browsers: [],
+				cascade: false,
 			},
-		],
-	},
-});
-
-mix.options({
-	autoprefixer: {
-		options: {
-			// browsers: [
-			// 	'last 2 versions',
-			// 	'IE 9',
-			// 	'Safari 8',
-			// ],
-			cascade: false,
 		},
-	},
-	cleanCss: {
-		// restructuring: false
-	},
-});
-
-mix.browserSync({
-	files: [
-		'public/assets',
-	],
-	ghostMode: false,
-	notify: false,
-	open: false,
-	port: 5757,
-	proxy: 'core.dev.com',
-});
+		clearConsole: false,
+		// cleanCss: {},
+	})
+	.webpackConfig({
+		module: {
+			rules: [
+				{
+					test: /\.scss$/,
+					use: [
+						{
+							loader: 'sass-loader',
+							options: {
+								importer,
+							},
+						},
+					],
+				},
+			],
+		},
+	});
 
 mix.sass('src/scss/main.scss', 'public/assets/css/');
