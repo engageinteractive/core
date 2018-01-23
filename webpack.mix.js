6b302908066c24073a8dc95a19eeb3b10c35fc08
Adam Gilleard <adam.gilleard@engageinteractive.co.uk>
2018-01-23 14:35:11 +0000

    Start on css task

---
+++ webpack.mix.js
@@ -0,0 +1,39 @@
+const mix = require('laravel-mix');
+const jsonImporter = require('node-sass-json-importer');
+
+mix.options({
+	clearConsole: false,
+});
+
+// mix.autoload({}); // TODO: check if needed
+
+mix.webpackConfig({
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: 'sass-loader',
+						options: {
+							importer: jsonImporter,
+						},
+					},
+				],
+			},
+		],
+	},
+});
+
+mix.browserSync({
+	"files": [
+		"public/assets",
+	],
+	"ghostMode": false,
+	"notify": false,
+	"open": false,
+	"port": 5757,
+	"proxy": "core.dev.com",
+})
+
+mix.sass('src/scss/main.scss', 'public/assets/css/');
