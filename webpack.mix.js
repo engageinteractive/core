e20befafe238d088569bd3045ee140d01a125b0a
Adam Gilleard <adam.gilleard@engageinteractive.co.uk>
2018-01-23 15:21:39 +0000

    Start js task

--- webpack.mix.js
+++ webpack.mix.js
@@ -42,3 +42,5 @@ mix
 	.sass('src/scss/main.scss', 'public/assets/css/')
 	.sass('src/scss/styleguide.scss', 'public/assets/css/')
 	.sass('src/scss/page/system-message.scss', 'public/assets/css/page/');
+
+mix.js('src/js/site.js', 'public/assets/js/');
